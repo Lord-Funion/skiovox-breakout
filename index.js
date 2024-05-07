a3ea040b45de5ba64ced43ceb369e37e54bda7bb
Lord-Funion <126121452+Lord-Funion@users.noreply.github.com>
2024-05-06 19:11:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ start.addEventListener("click", async function () {
 
 cancel.addEventListener("click", async function () {
     let msg = await chrome.runtime.sendMessage({ type: "cancel-inspect" });
-    if (!msg) return changeStatusMessage("failed! oh no! im gonna die!");
+    if (!msg) return changeStatusMessage("failed!");
     return changeStatusMessage("canceled");
 });
 sections().forEach(function (element) {
